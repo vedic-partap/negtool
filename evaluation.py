587f3629aef436901ac5ce50f51ae27bf8d08b09
marenger <marenger@ulrik.uio.no>
2017-01-23 14:12:22 +0100

    Added functions for development

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,4 @@
 import itertools
-from statsmodels.sandbox.stats.runs import mcnemar
-from statsmodels.stats.descriptivestats import sign_test
 from utils import get_affix_cue, count_multiword_cues, check_mw_start, convert_to_IO
 import numpy as np
 
@@ -33,7 +31,7 @@ def convert_cues_to_fileformat(sentences, labels, affix_cue_lexicon, filename, m
             outfile.write("\n")
         else:
             written_cue_on_line = False
-            for i in range(upper_limit): #NB ma byttes til 7 for cdd
+            for i in range(upper_limit): #NB change to 7 for CD-dataset
                 outfile.write("%s\t" %tokens[i])
             if n_cues == 0:
                 outfile.write("***\n")
@@ -42,11 +40,11 @@ def convert_cues_to_fileformat(sentences, labels, affix_cue_lexicon, filename, m
                     if labels[sent_counter][line_counter] < 0:
                         outfile.write("_\t_\t_\t")
                     else:
-                        #cue-linje.
+                        #cue-line
                         if written_cues[cue_i] or written_cue_on_line:
                             outfile.write("_\t_\t_\t")
                         else:
-                            #NB endre tilbake til tokens[3] for cdd
+                            #NB change to tokens[3] for CD-dataset
                             affix = get_affix_cue(tokens[1].lower(), affix_cue_lexicon)
                             if affix != None:
                                 outfile.write("%s\t_\t_\t" %affix)
@@ -113,88 +111,4 @@ def convert_scopes_to_fileformat(sentences, labels, filename, mode):
 
     infile.close()
     outfile.close()
-
-def sanity_check(gold, system):
-    with open(gold, 'r') as gold_file, open(system,'r') as sys_file:
-        line_counter = 0
-        i = 0
-        for line in gold_file:
-            sys_line = sys_file.readline().split()
-            gold_line = line.split()
-            if sys_line != gold_line:
-                if i < 30:
-                    print "Line number:", line_counter
-                i += 1
-            if len(gold_line) == 0 and i > 20 and i < 30:
-                print "\n"
-            line_counter += 1
-
-def compute_baseline():
-    infile = open("../data/gold/cde.txt", "r")
-    outfile = open("baseline_cdd.txt", "w")
-    n_cues = -1
-    for line in infile:
-        tokens = line.split()
-        if len(tokens) == 0:
-            outfile.write("\n")
-        elif tokens[-1] == "***":
-            outfile.write(line)
-        else:
-            n_cues = (len(tokens) - 9)/3 + 1
-            for i in range(7):
-                outfile.write("%s\t" %tokens[i])
-
-            for cue_i in range(n_cues):
-                outfile.write("%s\t" %tokens[7 + 3*cue_i]) #skriver gull-cue
-                if tokens[7+ 3*cue_i] == "_":
-                    outfile.write("%s\t" %tokens[3]) #alle tokens i setningen er in-scope unntatt cue
-                else:
-                    outfile.write("_\t")
-                outfile.write("%s\t" %tokens[9 + 3*cue_i]) #skriver event
-            outfile.write("\n")
-
-    infile.close()
-    outfile.close()
-
-def significance_test_scope(y1, y2, y_gold):
-    s1 = []
-    s2 = []
-    for i in range(len(y1)):
-        y1[i] = convert_to_IO(y1[i])
-        y2[i] = convert_to_IO(y2[i])
-        y_gold[i] = convert_to_IO(y_gold[i])
-        for j in range(len(y1[i])):
-            if y1[i][j] == y_gold[i][j]:
-                s1.append(1)
-            else:
-                s1.append(0)
-
-            if y2[i][j] == y_gold[i][j]:
-                s2.append(1)
-            else:
-                s2.append(0)
-        
-    print "Number of samples:", len(s1)
-    test = mcnemar(s1, s2)
-    print "P-value from McNemar-test:", test[1]
-
-def significance_test_cue(y1, y2, y_gold):
-    s1 = []
-    s2 = []
-    for i in range(len(y1)):
-        if y1[i] == y_gold[i]:
-            s1.append(1)
-        else:
-            s1.append(0)
-        if y2[i] == y_gold[i]:
-            s2.append(1)
-        else:
-            s2.append(0)
-
-    print "Number of samples:", len(s1)
-    test = mcnemar(s1, s2)
-    print "P-value from McNemar test:", test[1]
-
-if __name__ == "__main__":
-    compute_baseline()
             
